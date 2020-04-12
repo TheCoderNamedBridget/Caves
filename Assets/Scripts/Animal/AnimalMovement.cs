f6186fe398580ae5ecafe0bfcabfc5ba3b6066ec
Bridget <bridgetnaylor2000@gmail.com>
2020-04-12 14:13:59 -0700

    Crabs now move around completely randomly

--- Assets/Scripts/Animal/AnimalMovement.cs
+++ Assets/Scripts/Animal/AnimalMovement.cs
@@ -4,56 +4,84 @@ using UnityEngine;
 
 public class AnimalMovement : MonoBehaviour
 {
+ private float latestDirectionChangeTime;
+ private readonly float directionChangeTime = 3f;
+ private float characterVelocity = 2f;
+ private Vector2 movementDirection;
+ private Vector2 movementPerSecond;
+ 
+ 
+ void Start(){
+     latestDirectionChangeTime = 0f;
+     calcuateNewMovementVector();
+ }
+ 
+ void calcuateNewMovementVector(){
+    //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+     movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+     movementPerSecond = movementDirection * characterVelocity;
+ }
+ 
+ void Update(){
+     //if the changeTime was reached, calculate a new movement vector
+     if (Time.time - latestDirectionChangeTime > directionChangeTime){
+         latestDirectionChangeTime = Time.time;
+         calcuateNewMovementVector();
+     }
+     
+     //move enemy: 
+     transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), 
+     transform.position.y + (movementPerSecond.y * Time.deltaTime));
+ 
+ }
 
-    [SerializeField]
-    private float moveSpeed = 5f;
+
+
+    // [SerializeField]
+    // private float moveSpeed = 5f;
 
     
-    [SerializeField]
-    private GameObject Animal;
+    // [SerializeField]
+    // private GameObject Animal;
 
 
       
-    // Start is called before the first frame update
-    void Start()
-    {
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
 
-        if (Input.GetKey("d"))
-        {
-            print("right arrow key is held down");
-            Animal.GetComponent<SpriteRenderer>().flipX = true;
-            Animal.transform.position += new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
-        }
+    //     if (Input.GetKey("d"))
+    //     {
+    //         print("right arrow key is held down");
+    //         Animal.GetComponent<SpriteRenderer>().flipX = true;
+    //         Animal.transform.position += new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+    //     }
 
-        if (Input.GetKey("a"))
-        {
-            print("left arrow key is held down");
-            Animal.GetComponent<SpriteRenderer>().flipX = false;
-            Animal.transform.position += new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
-        }
+    //     if (Input.GetKey("a"))
+    //     {
+    //         print("left arrow key is held down");
+    //         Animal.GetComponent<SpriteRenderer>().flipX = false;
+    //         Animal.transform.position += new Vector3(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+    //     }
 
-        if (Input.GetKey("s")){
+    //     if (Input.GetKey("s")){
 
-            Debug.Log("Inside Vert");
-            Animal.transform.position += new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0);
+    //         Debug.Log("Inside Vert");
+    //         Animal.transform.position += new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0);
  
-        }
+    //     }
 
-        if (Input.GetKey("w")){
+    //     if (Input.GetKey("w")){
 
-            Debug.Log("Inside Vert");
-            Animal.transform.position += new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0);
+    //         Debug.Log("Inside Vert");
+    //         Animal.transform.position += new Vector3(0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0);
 
-        }
-
-
-        
-
-    }
+    //     }
+    // }
 }
