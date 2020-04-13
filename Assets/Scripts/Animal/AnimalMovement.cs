a48b30fc74ca1849f67419e0a78dde967670337b
Bridget <bridgetnaylor2000@gmail.com>
2020-04-13 08:15:40 -0700

    Added an animated grey shark swimming

--- Assets/Scripts/Animal/AnimalMovement.cs
+++ Assets/Scripts/Animal/AnimalMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AnimalMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject animal;
  private float latestDirectionChangeTime;
  private readonly float directionChangeTime = 3f;
  private float characterVelocity = 2f;
@@ -17,6 +20,8 @@ public class AnimalMovement : MonoBehaviour
  }
  
  void calcuateNewMovementVector(){
+
+
     //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
      movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
      movementPerSecond = movementDirection * characterVelocity;
